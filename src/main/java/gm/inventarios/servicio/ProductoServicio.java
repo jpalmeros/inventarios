92e0e5641ddcf85b151e9cdc4246295a98c66119
Jose Palmer <j_palmeros@yahoo.com.mx>
2024-03-17 20:43:56 -0600

    Creacion de la capa de Servicio y Datos

---
+++ src/main/java/gm/inventarios/servicio/ProductoServicio.java
@@ -0,0 +1,37 @@
+package gm.inventarios.servicio;
+
+import gm.inventarios.modelo.Producto;
+import gm.inventarios.repositorio.ProductoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoServicio implements IProductoServicio {
+
+    @Autowired
+    private ProductoRepositorio productoRepositorio;
+
+    @Override
+    public List<Producto> listarProductos() {
+        this.productoRepositorio.findAll();
+    }
+
+    @Override
+    public Producto buscarProductoId(Integer idProducto) {
+        Producto producto;
+        producto = this.productoRepositorio.findById(idProducto).orElse(null);
+        return producto;
+    }
+
+    @Override
+    public void guardarProducto(Producto producto) {
+        this.productoRepositorio.save(producto);
+    }
+
+    @Override
+    public void eliminarProducto(Integer idProducto) {
+        this.productoRepositorio.deleteById(idProducto);
+    }
+}
